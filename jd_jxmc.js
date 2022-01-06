aaffb972bd3c2d5f3307f241048d6cef09e9bb6e
liuranya <52848742+liuranya@users.noreply.github.com>
2022-01-06 22:06:20 +0800

    Update jd_jxmc.js

--- jd_jxmc.js
+++ jd_jxmc.js
@@ -908,8 +908,8 @@ function shareCodesFormat() {
     $.newShareCodes = []
     const readShareCodeRes = await readShareCode();
     if (readShareCodeRes && readShareCodeRes.code === 200) {
-      $.newShareCodes = [...new Set([...$.inviteCodeList, ...($.res || []), ...(readShareCodeRes.data || [])])];
-    } else {
+      //$.newShareCodes = [...new Set([...$.inviteCodeList, ...($.res || []), ...(readShareCodeRes.data || [])])];
+    //} else {
       $.newShareCodes = [...new Set([...$.inviteCodeList, ...($.res || [])])];
     }
     console.log(`\n您将要助力的好友${JSON.stringify($.newShareCodes)}`)
