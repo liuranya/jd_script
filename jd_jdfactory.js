4040a0675770ff5e679da831a1d8423fc374ef4f
liuranya <321082081@qq.com>
2022-01-20 12:09:19 +0800

    回

--- jd_jdfactory.js
+++ jd_jdfactory.js
@@ -676,7 +676,7 @@ function jdfactory_getHomeData() {
 function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
-    $.get({url: `https://transfer.nz.lu/ddfactory`, timeout: 10000}, (err, resp, data) => {
+    $.get({url: `https://api.jdsharecode.xyz/api/ddfactory/${randomCount}`, timeout: 10000}, (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
