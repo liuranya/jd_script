c4fbe140d6427525e9f8f088248134a2bd1320ab
liuranya <52848742+liuranya@users.noreply.github.com>
2022-01-21 23:56:36 +0800

    5G超级盲盒

--- jd_mohe.js
+++ jd_mohe.js
@@ -461,7 +461,8 @@ function getShareCode() {
       url: "https://raw.fastgit.org/zero205/updateTeam/main/shareCodes/jd_mohe.json",
       headers: {
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
-      }
+      },
+      "timeout": 10000
     }, async (err, resp, data) => {
       try {
         if (err) {
